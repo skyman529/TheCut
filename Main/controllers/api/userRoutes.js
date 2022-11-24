8d7aa89fc0682de3f99ea72aeb2bc6e6b2ce09b1
skyman529 <skyman529@gmail.com>
2022-11-23 20:00:29 -0700

    moved assets folder

--- Main/controllers/api/userRoutes.js
+++ Main/controllers/api/userRoutes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
-}
+})
 
 module.exports = router;
--- Main/models/Project.js
+++ Main/models/Project.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 
 class Project extends Model {}
 
-Project.init(
+Project.init()
  
 
 module.exports = Project;
