130255d1e1c3bfe38b194923873664c070312cfd
eugene isingizwe <eugeneisingizwe95@hotmail.com>
2022-11-30 19:00:09 -0700

    updated css

--- public/js/appointment.js
+++ public/js/appointment.js
@@ -1,5 +1,5 @@
 // save reference to important DOM elements
-const timeDisplayEl = $('#barber-display');
+// const timeDisplayEl = $('#barber-display');
 const barberDisplayEl = $('#barber-display');
 const barberModelEl = $('#barber-modal');
 const barberFormEl = $('#barber-form');
@@ -10,29 +10,19 @@ const dateInputEl = $('#date-input');
 
 
 // handle printing project data to the page
-function printBarberData(barberName, service, price, timeDate) {
+function printAppointmentInfo(selectedBarber, selectedSerice, haircutPrice, selectedDate) {
   const barberRowEl = $('<tr>');
 
-  const barberNameTdEl = $('<td>').addClass('p-2').text(barberName);
+  const barberNameTdEl = $('<td>').addClass('p-2').text(selectedBarber);
 
-  const baberberTypeTdEl = $('<td>').addClass('p-2').text(service);
+  const baberberTypeTdEl = $('<td>').addClass('p-2').text(selectedSerice);
 
-  const priceTdEl = $('<td>').addClass('p-2').text(price);
+  const priceTdEl = $('<td>').addClass('p-2').text(haircutPrice);
 
-  const dateTimeTdEl = $('<td>').addClass('p-2').text(timeDate);
-
-  // const daysToDate = moment(timeDate, 'MM/DD/YYYY').diff(moment(), 'days');
-  // const daysLeftTdEl = $('<td>').addClass('p-2').text(daysToDate);
-
-  // const totalEarnings = calculateTotalEarnings(price, daysToDate);
-
-  // You can also chain methods onto new lines to keep code clean
-  // const totalTdEl = $('<td>')
-  //   .addClass('p-2')
-  //   .text('$' + totalEarnings);
+  const dateTimeTdEl = $('<td>').addClass('p-2').text(selectedDate);
 
   const deleteBarberBtn = $('<td>')
-    .addClass('p-2 delete-project-btn text-center')
+    .addClass('p-2 delete-appointment-btn text-center')
     .text('X');
 
   // By listing each `<td>` constiable as an argument, each one will be appended in that order
@@ -41,8 +31,6 @@ function printBarberData(barberName, service, price, timeDate) {
     baberberTypeTdEl,
     priceTdEl,
     dateTimeTdEl,
-    // daysLeftTdEl,
-    // totalTdEl,
     deleteBarberBtn
   );
 
@@ -51,20 +39,14 @@ function printBarberData(barberName, service, price, timeDate) {
   barberModelEl.modal('hide');
 }
 
-// function calculateTotalEarnings(rate, days) {
-//   const dailyTotal = rate * 8;
-//   const total = dailyTotal * days;
-//   return total;
-// }
-
-function handleDeletedBarbers(event) {
+function handleDeleteAppointmentInfo(event) {
   console.log(event.target);
   const btnClicked = $(event.target);
   btnClicked.parent('tr').remove();
 }
 
 // handle barber form submission
-function handleBarberFormSumbit(event) {
+function handleAppointmentFormSubmission(event) {
   event.preventDefault();
 
   const barberNames = barberNameInputEl.val().trim();
@@ -72,14 +54,12 @@ function handleBarberFormSumbit(event) {
   const selectPrice = priceRateInputEl.val().trim();
   const selectTime = dateInputEl.val().trim();
 
-  printBarberData(barberNames, selectService, selectPrice, selectTime);
+  printAppointmentInfo(barberNames, selectService, selectPrice, selectTime);
   
 
   barberFormEl[0].reset();
 }
 
-barberFormEl.on('submit', handleBarberFormSumbit);
-barberDisplayEl.on('click', '.delete-project-btn', handleDeletedBarbers);
-dateInputEl.datepicker({ minDate: 1 });
+barberFormEl.on('submit', handleAppointmentFormSubmission);
+barberDisplayEl.on('click', '.delete-appointment-btn', handleDeleteAppointmentInfo);
 
-setInterval(displayTime, 1000);
